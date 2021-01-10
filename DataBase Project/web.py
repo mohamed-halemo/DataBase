fa5f31aea910ceec04160f980654a3e09af6b353
mohamed <>
2021-01-10 03:52:34 +0200

    Latest Update(login+register+thier validations)

--- DataBase Project/web.py
+++ DataBase Project/web.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect,request
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='b0a26ce6fb663a3d7f006e020de17a9e'  #secret key for protection
 
 @app.route('/')
 @app.route('/home')
@@ -9,6 +12,32 @@ def home():
 @app.route('/about')
 def about():
    return render_template('About.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+   form=RegistrationForm()
+
+   
+
+   if form.validate_on_submit():
+      if request.form.getlist('Doctor') :  ##knowing who entered a patient or a doctor
+         flash(f'Account Created for Doctor {form.username.data}!','success')  #if data is correct go back to home not same page
+         return redirect(url_for('home'))
+      elif request.form.getlist('Patient'):
+         flash(f'Account Created for MR/MS {form.username.data}!','success')  #if data is correct go back to home not same page
+         return redirect(url_for('home'))
+
+       
+   return render_template('register.html',title='Register',form=form)
+
+@app.route('/login', methods=['GET','POST'] )
+def login():
+   
+   form=LoginForm()
+   ##if form.validate_on_submit():
+      #### wait for data base 
+   return render_template('login.html',title='Login',form=form)
+
 
 if __name__ == '__main__':
    app.run(debug=True)
